c8003a23baabb61f6b3adbc42b66f21f3fc9e993
Tim Benjamin <tim@tienjin.com>
2022-11-24 17:44:42 +0000

    finished day 8

--- day_8/day_8.go
+++ day_8/day_8.go
@@ -2,67 +2,98 @@ package day_8
 
 import (
 	"adventofcode2018/util"
-	"fmt"
 	"strconv"
 )
 
 var input []int
-var metadata_count int
 
-func add_metadata(node []int) {
-	fmt.Printf("this segment: %v\n", node)
-	metadata := node[len(node)-(node[1]):]
-	for i := 0; i < len(metadata); i++ {
-		metadata_count += metadata[i]
+type Node struct {
+	num_child_nodes      int
+	num_metadata_entries int
+	child_nodes          []Node
+	metadata             []int
+}
+
+// parse the input into an easier tree structure
+func parse(input []int) ([]Node, []int) {
+	nodes := []Node{}
+	node := Node{}
+	node.num_child_nodes = input[0]
+	node.num_metadata_entries = input[1]
+
+	if node.num_child_nodes == 0 {
+		// 0 3 10 11 12 1 1 0 1 99 2
+		node.metadata = input[2 : 2+node.num_metadata_entries]
+		node.child_nodes = []Node{}
+		nodes = append(nodes, node)
+		remains := input[2+node.num_metadata_entries:]
+		return nodes, remains
+	} else {
+		// 2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+		// 1 1 0 1 99 2
+		node.child_nodes = []Node{}
+		remains := input[2:]
+		var child_nodes []Node
+		for i := 0; i < node.num_child_nodes; i++ {
+			child_nodes, remains = parse(remains)
+			node.child_nodes = append(node.child_nodes, child_nodes...)
+		}
+		node.metadata = remains[0:node.num_metadata_entries]
+		remains = remains[node.num_metadata_entries:]
+		nodes = append(nodes, node)
+		return nodes, remains
 	}
-	fmt.Printf("metadata count after this node: %v\n", metadata_count)
+}
+
+// sum a slice of ints
+func sum_metadata(metadata []int) (metadata_sum int) {
+	for i := 0; i < len(metadata); i++ {
+		metadata_sum += metadata[i]
+	}
 	return
 }
 
-func analyse(node []int) {
-	fmt.Printf("> Analysing... %v\n", node)
-	num_of_subnodes := node[0]
-	num_metadata := node[1]
-	metadata := node[(len(node) - num_metadata):]
-	fmt.Printf("num of subnodes: %v\n", num_of_subnodes)
-	fmt.Printf("num of metadatas: %v\n", num_metadata)
-	fmt.Printf("metadata: %v\n", metadata)
-
-	remains := node[2:(len(node) - num_metadata - 1)]
-	fmt.Printf("remains: %v\n", remains)
-
-	if num_of_subnodes > 0 {
-
-	} else {
-		//sub_metadata := remains
+// part 1 calculation
+func calculate_metadata_sum(node Node) {
+	s := sum_metadata(node.metadata)
+	metadata_sum += s
+	for _, c := range node.child_nodes {
+		calculate_metadata_sum(c)
 	}
-
-	analyse(remains)
 }
 
+// part 2 calculation
+func calculate_root_value(node Node) {
+	if node.num_child_nodes == 0 {
+		s := sum_metadata(node.metadata)
+		root_value += s
+	} else {
+		for _, m := range node.metadata {
+			if m > node.num_child_nodes || m == 0 {
+				// ignore as it is out of bounds
+			} else {
+				calculate_root_value(node.child_nodes[m-1])
+			}
+		}
+	}
+}
+
+var metadata_sum int // part 1 answer
 func part_1() int {
-	metadata_count = 0
-	// for i := 0; i < len(input); i++ {
-	// 	// first i will be a header
-	// 	fmt.Printf("header: %v\n", i)
-	// 	// this tells us how many nodes there are in this section
-	// 	// need a function to get each node
-	// 	// (that will be recursively called!)
-	// 	// but for this part we can simply recursively call and simply add the metadata in each node that is found in some global variable
-	// }
-
-	//add_metadata(input)
-	analyse(input)
-
-	// maybe recurse down until we get nodes with 0 as the number of sub-nodes
-	// and eliminate those from the input
-
-	return metadata_count
+	metadata_sum = 0
+	nodes, _ := parse(input)
+	root_node := nodes[0]
+	calculate_metadata_sum(root_node)
+	return metadata_sum
 }
 
+var root_value int // part 2 answer
 func part_2() int {
-
-	return 0
+	root_value = 0
+	nodes, _ := parse(input)
+	root_node := nodes[0]
+	calculate_root_value(root_node)
+	return root_value
 }
 
 func Call(part string, input_file string) string {
